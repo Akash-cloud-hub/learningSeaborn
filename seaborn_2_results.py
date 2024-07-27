1902acb2b4ea7153b9f651dceebb8595e73f767e
Akash <airshula79@gmail.com>
2024-07-27 21:37:01 +0700

    seaborn

--- seaborn_2_results.py
+++ seaborn_2_results.py
@@ -10,4 +10,19 @@ df = sns.load_dataset('iris')
 ic(df.head())
 
 sns.catplot(x='species' , y='sepal_length' , data = df , kind='violin' , hue='species')
+# plt.show()
+plt.clf()
+
+
+
+#homework
+sns.catplot(x='species' , y='sepal_length' , data = df , kind='swarm' , hue='species')
+# plt.show()
+plt.clf()
+
+
+sns.set()
+df = sns.load_dataset('tips')
+ic(df.head())
+sns.catplot(x='day' , y='total_bill' , data = df , kind='violin' , hue="sex" )
 plt.show()
